91ed5d9e8307d2459d9ff86dacafb366a9837461
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2021-07-04 13:35:04 +0200

    Work on project. Stage 1/4: Simplest estimation

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,7 +1,15 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        if (text.length() > 100) {
+            System.out.println("HARD");
+        } else {
+            System.out.println("EASY");
+        }
     }
 }
